37ea287bff74d77a847b2224f2a8660388471f59
Luis-Felipe-Santos <luysfelyp07@gmail.com>
2024-12-31 11:44:36 -0300

    Iniciando o projeto

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <TheHeader />
+    <router-view />
+    <TheFooter />
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader.vue";
+import TheFooter from "./components/TheFooter.vue";
+
+export default {
+  name: "App",
+  components: TheHeader,
+  TheFooter,
+};
+</script>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p>Frete grátis a partir de R$ 199,99 para todo o Brasil!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheHeader",
+};
+</script>
